b7ecef61d2f46124e4b261327d9fa85bd8f6ef04
maristvs <mariannalves5zoo@gmail.com>
2023-05-01 20:52:28 -0300

    alteração Cipher

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+function cifrarMensagem() {
+  const mensagem = document.getElementById('mensagem').value.trim();
+  const chave = parseInt(document.getElementById('chave').value);
+  const mensagemCifrada = cipher.encode(chave, mensagem);
+  document.getElementById('mensagem-cifrada').innerText = mensagemCifrada;
+}
+
+const btnCifrar = document.getElementById('btn-cifrar');
+btnCifrar.addEventListener('click', cifrarMensagem);
+
+function decifrarMensagem() {
+  const mensagemCifrada = document.getElementById('input-decifrar').value.trim();
+  const chave = parseInt(document.getElementById('chave1').value);
+  const mensagemDecifrada = cipher.decode(chave, mensagemCifrada);
+  document.getElementById('resultado-decifragem').innerText = mensagemDecifrada;
+}
+
+const btnDecifrar = document.getElementById('btn-decifrar');
+btnDecifrar.addEventListener('click', decifrarMensagem);
