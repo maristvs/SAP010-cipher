a4b5ec4144e1cf1bbfaa795ef33d39fb19555682
maristvs <mariannalves5zoo@gmail.com>
2023-05-03 20:43:32 -0300

    readpronto

--- src/cipher.js
+++ src/cipher.js
@@ -1,18 +1,22 @@
-const cipher = {encode, decode}
+// Define um objeto chamado 'cipher' que contém as funções 'encode' e 'decode'
+const cipher = { encode, decode }
 
-
+// Define a função 'encode' que recebe um deslocamento (offset) e uma mensagem inserida
 function encode(offset, mensagem) {
   let result = '';
+    // Verifica se o tipo da mensagem não é uma string ou se o tipo do offset não é um número
   if (typeof mensagem !== "string" || typeof offset !== "number") {
+        // Lança um erro do tipo TypeError caso a verificação acima seja verdadeira
     throw new TypeError();
   }
+   // Percorre cada caractere da mensagem
   for (let i = 0; i < mensagem.length; i++) {
     const executa = ((mensagem.charCodeAt(i) - 65 + offset) % 26) + 65;
     result += String.fromCharCode(executa);
   }
   return result;
 }
-
+// Define a função 'decode' que recebe um deslocamento (offset) e uma mensagem cifrada
 function decode(offset, mensagemCifrada) {
   let result = "";
   if (typeof mensagemCifrada !== "string" || typeof offset !== "number") {
@@ -24,5 +28,5 @@ function decode(offset, mensagemCifrada) {
   }
   return result;
 }
-
+// Exporta o objeto 'cipher'para que possa ser importado em outros arquivos
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,26 @@
+// Importa o objeto 'cipher' do arquivo 'cipher.js'
 import cipher from './cipher.js';
 
+// Define a função 'cifrarMensagem' para cifrar a mensagem
 function cifrarMensagem() {
   const mensagem = document.getElementById('mensagem').value.trim();
   const chave = parseInt(document.getElementById('chave').value);
+    // Cifra a mensagem utilizando a função 'encode' do objeto 'cipher' com a chave fornecida
   const mensagemCifrada = cipher.encode(chave, mensagem);
   document.getElementById('mensagem-cifrada').innerText = mensagemCifrada;
 }
-
+// Aqui está o elemento HTML do botão de cifrar com o id 'btn-cifrar'
 const btnCifrar = document.getElementById('btn-cifrar');
-btnCifrar.addEventListener('click', cifrarMensagem);
+// Adiciona um ouvinte de evento de clique ao botão de cifrar que chama a função 'cifrarMensagem'
+btnCifrar.addEventListener('click', cifrarMensagem);  
 
+// Define a função 'decifrarMensagem' para decifrar a mensagem
 function decifrarMensagem() {
   const mensagemCifrada = document.getElementById('input-decifrar').value.trim();
   const chave = parseInt(document.getElementById('chave1').value);
   const mensagemDecifrada = cipher.decode(chave, mensagemCifrada);
   document.getElementById('resultado-decifragem').innerText = mensagemDecifrada;
 }
-
+// E aqui está o elemento HTML do botão de cifrar com o id 'btn-decifrar'
 const btnDecifrar = document.getElementById('btn-decifrar');
 btnDecifrar.addEventListener('click', decifrarMensagem);
